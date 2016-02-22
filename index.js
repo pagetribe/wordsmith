db0f4ba6891aa22ec3105dae747d293c944640d3
pagetribe <pagetribemail@gmail.com>
2016-02-22 20:37:55 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var synonyms = [{"synonyms": ["anarchical", "lawless"], "word": "anarchic", "numberOfSynonyms": 2},
+ 								{"synonyms": ["anarchic", "lawless"], "word": "anarchical", "numberOfSynonyms": 2}]
